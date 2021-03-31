1738986b743707d8bf273263dfb3b2fb4a2ad22d
Gregorio Maclen <gregorio.maclen@gmail.com>
2021-03-31 14:59:06 -0300

    Actualizacion

---
+++ AyED/src/p2ej1/Inciso5.java
@@ -0,0 +1,16 @@
+package p2ej1;
+
+public class Inciso5 {
+	
+	public static void imprimirListaInverso(NodoEntero nodo) {
+		if (nodo != null) {
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ AyED/src/p2ej1/TestListaDeEnterosEnlazada.java
@@ -0,0 +1,32 @@
+package p2ej1; // bien
+
+import java.util.Scanner;
+
+import ListasDeEnteros.src.tp02.ejercicio1.copy.ListaDeEnterosEnlazada;
+
+public class TestListaDeEnterosEnlazada {
+
+	public static void main(String[] args) {
+		Integer valor;
+		Scanner s = new Scanner(System.in);
+		
+		ListaDeEnterosEnlazada lista = new ListaDeEnterosEnlazada();
+		
+		System.out.println("Ingrese el valor (0 para finalizar): ");
+		valor = s.nextInt();
+		while (valor != 0) {
+			if (lista.agregarFinal(valor)) {
+				System.out.println("Ingrese el valor (0 para finalizar): ");
+				valor = s.nextInt();
+			}
+		}
+		
+		int i = 1;
+		while (lista.elemento(i) != null) {
+			System.out.println("Elemento de la posicion " + i + ": " + lista.elemento(i));
+			i++;
+		}
+
+	}
+
+}
