e8e76698b98e01fa7917f6927b5f5029c3d6ebb0
Gregorio Maclen <gregorio.maclen@gmail.com>
2021-04-23 12:29:54 -0300

    .

---
+++ AyED/src/p3ej5/ProfundidadArbolBinario.java
@@ -0,0 +1,42 @@
+package p3ej5;
+
+import ListasGenericas.src.tp02.ejercicio2.copy.ColaGenerica;
+import tp03_ab.tp03.ejercicio1.ArbolBinario;
+
+public class ProfundidadArbolBinario {
+	ArbolBinario<Integer> arbol;
+	
+	public ProfundidadArbolBinario(ArbolBinario<Integer> arbol) {
+		this.arbol = arbol;
+	}
+	
+	public ArbolBinario<Integer> getArbol() {
+		return arbol;
+	}
+	
+	public int sumaElementosProfundidad(int p) { //creo que bien. hay que probarlo
+		int suma = 0;
+		int nivel = 0;
+		ArbolBinario<Integer> aux = null;
+		ColaGenerica<ArbolBinario<Integer>> cola = new ColaGenerica<ArbolBinario<Integer>>();
+		cola.encolar(this.getArbol());
+		cola.encolar(null);
+		while (nivel < p && !cola.esVacia()) {
+			aux = cola.desencolar();
+			if (arbol != null) {
+				suma = suma + aux.getDato();
+				if (aux.tieneHijoIzquierdo()) {
+					cola.encolar(aux.getHijoIzquierdo());
+				}
+				if (aux.tieneHijoDerecho()) {
+					cola.encolar(aux.getHijoDerecho());
+				}
+			}
+			else if (!cola.esVacia()) {
+				cola.encolar(null);
+				nivel++;
+			}
+		}
+		return suma;
+	}
+}
