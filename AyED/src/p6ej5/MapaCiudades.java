9db72a99971503edb7a31e6ffe24b5888a92a5bc
Gregorio Maclen <gregorio.maclen@gmail.com>
2021-06-18 18:48:45 -0300

    .

--- AyED/src/p6ej5/MapaCiudades.java
+++ AyED/src/p6ej5/MapaCiudades.java
@@ -14,26 +14,61 @@ public class MapaCiudades<T> {
 		this.grafo = grafo;
 	}
 	
-	public ListaGenerica<String> devolverCamino(String ciudad1, String ciudad2){
+	public ListaGenerica<String> devolverCamino(String ciudad1, String ciudad2){ //consultar si esta bien
 		boolean[] marca = new boolean[this.grafo.listaDeVertices().tamanio()+1];
 		ListaGenerica<String> lista = new ListaEnlazadaGenerica<String>();
 		for (int i = 0; i < this.grafo.listaDeVertices().tamanio(); i++) {
-			if (!marca[i])
-				this.devolverCamino(i, this.grafo, lista, marca);
+			if (!marca[i] && this.grafo.listaDeVertices().elemento(i).dato().equals(ciudad1))
+				this.devolverCamino(ciudad2, i, this.grafo, lista, marca);
 		}
 		return lista;
 	}
 	
-	private void devolverCamino (int i, Grafo<String> grafo, ListaGenerica<String> lista, boolean[] marca){
+	private boolean devolverCamino (String ciudad2, int i, Grafo<String> grafo, ListaGenerica<String> lista, boolean[] marca){
 		marca[i] = true;
 		Vertice<String> v = grafo.listaDeVertices().elemento(i);
-		lista.agregarFinal(v.dato());
 		ListaGenerica<Arista<String>> ady = grafo.listaDeAdyacentes(v);
 		ady.comenzar();
 		while (!ady.fin()){
 			int j = ady.proximo().getVerticeDestino().getPosicion();
-			if (!marca[j])
-				this.devolverCamino(j, grafo, lista, marca);
+			if (!marca[j]) {
+				if (v.dato().equals(ciudad2) || this.devolverCamino(ciudad2, j, grafo, lista, marca)){
+					lista.agregarInicio(v.dato());
+					return true;
+				}
+			}
 		}
+		return false;
+	}
+	
+	public ListaGenerica<String> devolverCaminoExceptuando(String ciudad1, String ciudad2, ListaGenerica<String> ciudades){ //consultar si esta bien
+		boolean[] marca = new boolean[this.grafo.listaDeVertices().tamanio()+1];
+		ListaGenerica<String> lista = new ListaEnlazadaGenerica<String>();
+		for (int i = 0; i < this.grafo.listaDeVertices().tamanio(); i++) {
+			if (!marca[i] && this.grafo.listaDeVertices().elemento(i).dato().equals(ciudad1))
+				this.devolverCaminoExceptuando(ciudad2, ciudades, i, this.grafo, lista, marca);
+		}
+		return lista;
+	}
+	
+	private boolean devolverCaminoExceptuando (String ciudad2, ListaGenerica<String> ciudades, int i, Grafo<String> grafo, ListaGenerica<String> lista, boolean[] marca){
+		marca[i] = true;
+		Vertice<String> v = grafo.listaDeVertices().elemento(i);
+		for (int j = 0; j < ciudades.tamanio(); j++) {
+			if (v.dato().equals(ciudades.elemento(j)))
+				return false;
+		}
+		ListaGenerica<Arista<String>> ady = grafo.listaDeAdyacentes(v);
+		ady.comenzar();
+		while (!ady.fin()){
+			int j = ady.proximo().getVerticeDestino().getPosicion();
+			if (!marca[j]) {
+				if (v.dato().equals(ciudad2) || this.devolverCaminoExceptuando(ciudad2, ciudades, j, grafo, lista, marca)){
+					lista.agregarInicio(v.dato());
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
